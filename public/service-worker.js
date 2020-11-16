b5d644765cc7170ecbbaa9141902771cfb2ad956
Amanda Anderson <aanderson_120@yahoo.com>
2020-11-16 15:06:38 -0600

    update service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,18 @@
 console.log("Hello from service worker!")
 
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
 const FILES_TO_CACHE = [
 	'/',
 	'/index.html',
 	'/index.js',
+	'/db.js',
 	'/manifest.webmanifest',
 	'/styles.css',
 	'/icons/icon-192x192.png',
 	'/icons/icon-512x512.png'
 ];
-
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
 self.addEventListener("install", function (evt) {
